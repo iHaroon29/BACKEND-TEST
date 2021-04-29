2ebec5e6579109cfaf5682571bae406423590a01
Dhruva Vig <digitaldhruv33@gmail.com>
2021-04-29 14:21:54 +0530

    route to get all course section

--- LMS/routes/Students.js
+++ LMS/routes/Students.js
@@ -1,3 +1,20 @@
 const router=require('express').Router();
+const CourseSections = require('../models/courseSections')
+require("./RouteMiddlewares");
+const op = require('sequelize')
+
+
+router.get('/', (req, res) => {
+     CourseSections.findAll().then((data) => {
+      return  res.send(data).status(202)
+    }).catch((err) => {console.log(err)})
+
+    
+
+})
+
+
+
+
 
 module.exports=router;
