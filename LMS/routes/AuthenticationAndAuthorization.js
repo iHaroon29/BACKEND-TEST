a77517c2d45c1ad354789a237229de18af4ed9cc
dipu kr sah <dipuxah7@gmail.com>
2021-05-01 15:25:14 +0530

    initialized auth functions for students and teachers

--- LMS/routes/AuthenticationAndAuthorization.js
+++ LMS/routes/AuthenticationAndAuthorization.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const router = express.Router();
-
+const authTokens=require("../modules/authTokens");
+const validator=require("./DataValidators");
 
 router.post('/login/student', function(req, res) {
-    console.log(req);
-    return res.send(req.body).status(200);
+//    return auth token on correct details of the student
+
+});
+router.post('/login/teacher', function(req, res) {
+//    return auth token on correct details of the student
+
 });
 
 module.exports = router;
