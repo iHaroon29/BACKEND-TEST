a7233e781287e8457f594153fbc518c63e6a4854
dipu kr sah <dipuxah7@gmail.com>
2021-05-03 15:22:23 +0530

    controllers work initiated in LMS

---
+++ LMS/controllers/TeacherController.js
@@ -0,0 +1,10 @@
+const Assignment=require("../models/mongodb/assignments");
+const NewAssignmentValidator=require("../routes/DataValidators");
+
+module.exports={
+    addNewAssignment(assignmentDetails){
+
+    }
+
+
+};
