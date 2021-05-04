300b2c56fe9a38f072359fd1c22cd62f9539ff45
dipu kr sah <dipuxah7@gmail.com>
2021-05-04 13:45:45 +0530

    Hr Advisor Controller some function completed

--- HRMS/controllers/HrAdvisorController.js
+++ HRMS/controllers/HrAdvisorController.js
@@ -1,4 +1,9 @@
 const HrActivityController=require("./HrActivityController").ControllerClass;
+const NewHrApplicants=require("../models/mongodb/newHrApplicants");
+const HrRooms=require("../models/mongodb/HrRooms");
+const Classrooms=require("../models/mongodb/classrooms");
+const Teachers=require("../models/mongodb/teachers");
+
 
 class HrAdvisorControllerClass extends HrActivityController{
     addNewCallback() {
@@ -8,28 +13,76 @@ class HrAdvisorControllerClass extends HrActivityController{
 
     }
     getAllNewHrApplicants(){
-
+        return NewHrApplicants.find();
     }
-    exitHrRoom(){
+    exitHrRoom(data){
+        return new Promise((resolve, reject) => {
+            this.addNewHrActivity(data)
+                .then(()=>{
+                    return HrRooms.findOneAndDelete({hr_room_members:data.HrId})
+                }).catch(()=>{
+
+            })
+        })
 
     }
     getHrRoom(){
 
     }
-    getAllRoomInWhichAddedAsMember(){
+    getAllRoomInWhichAddedAsMember(data){
+        return HrRooms.find({hr_room_members:data.hr_id});
 
     }
-    addRoomComment(){
+    addRoomComment(details) {
+        this.addNewHrActivity()
+            .then(()=>{
+
+                if(!this.isHrIsEnrolledInRoom(details.hr_id,details.room_id))
+                    return;
+
+
+
+            }).catch(() => {
+        });
 
     }
+
     addTeacherToClass(){
 
     }
     getAllClassrooms(){
+        return Classrooms.find();
 
     }
-    getAllRoomComments(){
+    getAllRoomComments(details){
+       if(this.isHrIsEnrolledInRoom(details.hr_id,details.room_id)){
+           return HrRooms.findById(details.room_id);
+       }
 
+       return new Promise(((resolve, reject) => {
+           reject("not a member of the room");
+       }))
+
+    }
+    getAllAvailableTeachers(){
+        return Teachers.find({is_available:true});
+
+    }
+    isHrIsEnrolledInRoom(hrId,roomId){
+        return new Promise(((resolve, reject) => {
+            this.getAllRoomInWhichAddedAsMember({hr_id:hrId})
+                .then(rooms=>{
+                    for (let i = 0; i <rooms.length ; i++) {
+                        if(rooms[i]._id===roomId)
+                        {
+                            return true;
+                        }
+                    }
+                    return  false;
+                }).catch(roomFindingError=>{
+                reject(roomFindingError);
+            })
+        }))
     }
 
 }
