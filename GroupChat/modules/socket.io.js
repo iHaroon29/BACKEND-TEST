193c244bc4ae58aa1b8072325f615dd0abd7357a
dipu kr sah <dipuxah7@gmail.com>
2021-05-18 11:58:56 +0530

    socket module optimization work

---
+++ GroupChat/modules/socket.io.js
@@ -0,0 +1,7 @@
+const server=require("./httpServer");
+const socket=require("socket.io");
+const io=socket(server);
+io.on("connection",socket=>{
+    console.log(socket)
+});
+module.exports=socket;
