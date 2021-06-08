46d46b5d5205200aefd68f0221d32bf2b8ead4f4
dipu kr sah <dipuxah7@gmail.com>
2021-06-08 15:09:19 +0530

    lecture controller work finalized

---
+++ utils/lecturesvalidtors.js
@@ -0,0 +1,27 @@
+const Joi=require("joi");
+
+module.exports={
+    addNewLectureToClassroom(lectureDetails){
+        return Joi.object({
+            'classroom_id':Joi.string().required(),
+            "course_id":Joi.string().required(),
+            'date_and_time':Joi.date().required(),
+            'is_attendance_marked':Joi.boolean().optional().default(false),
+            'crm_meeting_link':Joi.string().required(),
+            'status':Joi.string().required(),
+            'is_active':Joi.boolean().optional().default(true)
+        }).options({stripUnknown:true}).validateAsync(lectureDetails);
+
+    },
+    updateLecture(lectureDetails){
+        return Joi.object({
+            'classroom_id':Joi.string().optional(),
+            "course_id":Joi.string().optional(),
+            'date_and_time':Joi.date().optional(),
+            'is_attendance_marked':Joi.boolean().optional(),
+            'crm_meeting_link':Joi.string().optional(),
+            'status':Joi.string().optional(),
+            'is_active':Joi.boolean().optional()
+        }).options({stripUnknown:true}).validateAsync(lectureDetails);
+    }
+};
