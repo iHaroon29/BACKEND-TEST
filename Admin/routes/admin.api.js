7f6021d296c8f7c59e2a7b97031cdcebb83eab5b
dipu kr sah <dipuxah7@gmail.com>
2021-06-08 15:10:29 +0530

    added routes of classroom and lectures

--- Admin/routes/admin.api.js
+++ Admin/routes/admin.api.js
@@ -3,11 +3,18 @@ const CourseController=require("../controllers/Courses");
 const CourseSectionController=require("../controllers/CourseSection");
 const AdminController=require("../controllers/admin.controller");
 const ClassroomController=require("../controllers/classroom.controller");
+const LectureController=require("../controllers/lectures.controller");
+
+
+//============= COURSES==================
 route.get("/course/all",CourseController.getAllCourse);
 route.post("/course/new",CourseController.addNewCourse);
 route.put("/course/all",CourseController.updateCourseById);
 route.delete("/course/:id",CourseController.deleteCourseById);
 route.post("/course_section/new",CourseSectionController.addNewCourseSection);
+
+//============= COURSES==================
+
 //============= ADMIN==================
 route.put("/forget/password",AdminController.updatePassword);
 route.delete("/admin/delete/:id",AdminController.deleteAdmin);
@@ -20,19 +27,35 @@ route.post("/classroom/new",ClassroomController.createNewClassroom);
 route.delete("/classroom/:classroomId",ClassroomController.deleteClassroomWithGivenId);
 route.get("/classroom/all",ClassroomController.getAllClassroom);
 route.post("/classroom/demo/teacher/new/:classroomId",ClassroomController.teacherForDemoClass);
+route.put("/classroom/update/:classroomId",ClassroomController.updateClassroomDetails);
 //==================== CLASSROOM================
 
-const Classroom=require("../../models/mongodb/classrooms");
 
-route.get("/test",(req,res)=>{
-    const studentID=1;
-    const dN={};
-    dN["enrolled_students."+studentID]={$exists:true};
-    console.log(dN);
-    Classroom.find(dN)
-        .then(data=>{
-            return res.status(200).send(data);
-        })
-});
+//======================= LECTURE=====================
+route.get("/classroom/:classroomId/lecture/all",LectureController.getAllLecturesOfClassroom);
+route.get("/classroom/:classroomId/lecture/:lectureId",LectureController.getLectureDetails);
+route.post("/classroom/:classroomId/lecture/new",LectureController.addNewLecture);
+route.put("/classroom/:classroomId/lecture/:lectureId",LectureController.updateLecture);
+route.delete("/classroom/:classroomId/lecture/:lectureId",LectureController.deleteLecture);
+
+//======================= LECTURE=====================
+
+
+
+
+
+
+// const Classroom=require("../../models/mongodb/classrooms");
+//
+// route.get("/test",(req,res)=>{
+//     const studentID=1;
+//     const dN={};
+//     dN["enrolled_students."+studentID]={$exists:true};
+//     console.log(dN);
+//     Classroom.find(dN)
+//         .then(data=>{
+//             return res.status(200).send(data);
+//         })
+// });
 
 module.exports=route;
