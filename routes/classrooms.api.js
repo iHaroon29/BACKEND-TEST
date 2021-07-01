6b7f4a31c7e0a1891f9bb6d6c26411480e2aa3f5
dipu kr sah <dipuxah7@gmail.com>
2021-07-01 10:07:37 +0530

    get classroomById support enables

--- routes/classrooms.api.js
+++ routes/classrooms.api.js
@@ -3,6 +3,7 @@ const route = require("express").Router();
 
 route.post("/classroom/new", ClassroomController.createNewClassroom);
 route.delete("/classroom/:classroomId",ClassroomController.deleteClassroomWithGivenId);
+route.get("/classroom/:classroomId",ClassroomController.getClassroomByClassroomId);
 route.get("/classroom/all", ClassroomController.getAllClassroom);
 route.post("/classroom/demo/teacher/new/:classroomId",ClassroomController.teacherForDemoClass);
 route.put("/classroom/update/:classroomId",ClassroomController.updateClassroomDetails);
