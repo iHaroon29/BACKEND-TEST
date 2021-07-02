39c053b03481e8ffde1d24c1e98e9e8ffb70bcb4
dipu kr sah <dipuxah7@gmail.com>
2021-07-02 21:21:51 +0530

    routes optimized

--- routes/course.sections.api.js
+++ routes/course.sections.api.js
@@ -1,11 +1,11 @@
 const route = require("express").Router();
 const CourseSectionController = require("../controllers/course.section.controller");
 
-route.get("/course/:courseId/section/all",CourseSectionController.getAllCourseSectionsOfCourse);
-route.get("/course/:courseId/section/:courseSectionId",CourseSectionController.getCourseSectionDetails);
-route.get("/course/section/all", CourseSectionController.getAllCourseSections);
+route.get("/course/:courseId/sections/all",CourseSectionController.getAllCourseSectionsOfCourse);
+route.get("/course/:courseId/section/details/:courseSectionId",CourseSectionController.getCourseSectionDetails);
+// route.get("/course/sections/all", CourseSectionController.getAllCourseSections);
 route.post("/course/:courseId/section/new",CourseSectionController.addNewCourseSection);
-route.put("/course/:courseId/section/:courseSectionId",CourseSectionController.updateCourseSection);
-route.delete("/course/:courseId/section/:courseSectionId",CourseSectionController.deleteCourseSection);
+route.put("/course/:courseId/section/update/:courseSectionId",CourseSectionController.updateCourseSection);
+route.delete("/course/:courseId/section/delete/:courseSectionId",CourseSectionController.deleteCourseSection);
 
 module.exports = route;
--- routes/courses.api.js
+++ routes/courses.api.js
@@ -1,8 +1,8 @@
 const route = require("express").Router();
 const CourseController = require("../controllers/course.controllers");
-route.get("/course/all", CourseController.getAllCourse);
+route.get("/courses/all", CourseController.getAllCourse);
 route.post("/course/new", CourseController.addNewCourse);
-route.get("/course/:courseId", CourseController.getCourseByCourseId);
-route.put("/course/:courseId/update", CourseController.updateCourseById);
-route.delete("/course/:id", CourseController.deleteCourseById);
+route.get("/course/details/:courseId", CourseController.getCourseByCourseId);
+route.put("/course/update/:courseId", CourseController.updateCourseById);
+route.delete("/course/delete/:id", CourseController.deleteCourseById);
 module.exports = route;
