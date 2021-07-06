3b4ec9ab0ad4ca161f07ca7e0ff936b6a738efc0
dipu kr sah <dipuxah7@gmail.com>
2021-07-06 11:01:30 +0530

    course and course section dao completed

---
+++ dao/course.dao.js
@@ -0,0 +1,68 @@
+const Course=require("../models/courses.model");
+const DAOError=require("../errors/dao.errors").getDAOErrorMessage;
+const CourseSectionDao=require("./course.section.dao");
+module.exports={
+    createNewCourse(courseDetails){
+        return new Promise((resolve,reject)=>{
+            new Course(courseDetails)
+                .then((savedCourse)=>{
+                    resolve(savedCourse)
+                }).catch(err=>{
+                    reject(DAOError("unable to add course",503,err));
+            })
+        })
+    },
+    updateCourseByCourseId(courseId,newCourseDetails){
+        return new Promise((resolve,reject)=>{
+            Course.findByIdAndUpdate(courseId,newCourseDetails,{new:true})
+                .then((updatedCourse)=>{
+                    resolve(updatedCourse)
+                }).catch(err=>{
+                reject(DAOError("unable to update course",503,err));
+            })
+        })
+    },
+    deleteCourseByCourseId(courseId){
+        return new Promise((resolve,reject)=>{
+            Course.findByIdAndDelete(courseId)
+                .then((updatedCourse)=>{
+                    resolve(updatedCourse)
+                }).catch(err=>{
+                reject(DAOError("unable to delete course",503,err));
+            })
+        })
+    },
+    getCourseByCourseId(courseId){
+        return new Promise((resolve,reject)=>{
+            Course.findById(courseId)
+                .then((courseDetails)=>{
+                    if(!courseDetails){
+                        reject(DAOError("no courses found",400));
+                    }
+                    courseDetails=JSON.parse(JSON.stringify(courseDetails));
+                    CourseSectionDao.getCourseSectionByCourseId(courseId).then((courseSections)=>{
+                        courseDetails.course_sections=courseSections;
+                        resolve(courseDetails)
+
+                    })
+                }).catch(err=>{
+                reject(DAOError("unable to delete course",503,err));
+            })
+        })
+    },
+    getAllCourses(){
+        return new Promise((resolve,reject)=>{
+            Course.find()
+                .then((allCourses)=>{
+                    allCourses=JSON.parse(JSON.stringify(allCourses));
+                    allCourses.filter(async (courseDetails,index)=>{
+                        courseDetails[index].sections=await CourseSectionDao.getCourseSectionByCourseId(courseDetails._id)
+                            .catch();
+                        return courseDetails;
+                    });
+                }).catch(err=>{
+                reject(DAOError("unable to delete course",503,err));
+            })
+        })
+    }
+};
