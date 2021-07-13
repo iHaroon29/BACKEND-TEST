c86f635860a646bf0d0b8360b8a6eedc118e8d62
dipu kr sah <dipuxah7@gmail.com>
2021-07-13 20:42:24 +0530

    course_id field removed lecture attendance validators

--- validators/lecture.attendance.validators.js
+++ validators/lecture.attendance.validators.js
@@ -4,7 +4,7 @@ module.exports={
     addNewAttendance(lectureAttendanceDetails){
         return Joi.object({
             student_id:Joi.string().lowercase().required(),
-            course_id:Joi.string().lowercase().required(),
+            // course_id:Joi.string().lowercase().required(),
             lecture_id:Joi.string().lowercase().required(),
         }).options({
             stripUnknown:true
